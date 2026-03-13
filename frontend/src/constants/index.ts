24401f8b5492029beaefc10bc10b13700abf1498
고혜연 <hyko@saluscare.co.kr>
2026-03-13 15:48:19 +0900

    Fix(cors): allow all origins, remove unused mock data files

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,4 +1,2 @@
-export { BLOG_POSTS } from "./home/mockBlog";
-export { PROJECTS } from "./home/mockProjects";
 export { LIVE_ACTIVITY, CURRENT_READING } from "./home/mockActivity";
 export { Icons } from "./icons";
